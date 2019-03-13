3ff936d7f0609bc3af410bdb3c9d9fd06472eaf9
v7Meluha <varshu.md@gmail.com>
2019-03-13 12:15:28 +0530

    make it better

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var Bot = require("./getResponse");
+var path = require("path")
 
 app.use(bodyParser.json());
 
 app.use(express.json());
 
-app.post("/", (req, res) => {
+/*app.post("/", (req, res) => {
   var action = req.body.queryResult.action;
   var params = req.body.queryResult.parameters;
   var news = Bot.getResponse(action, params)
@@ -17,5 +18,12 @@ app.post("/", (req, res) => {
     .catch(function(err) {
       res.json(err);
     });
+});*/
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.listen(8000, () => console.log("starting server"));
+
+app.listen(process.env.PORT || 8000, function(){
+    console.log('Your node js server is running');
+});
